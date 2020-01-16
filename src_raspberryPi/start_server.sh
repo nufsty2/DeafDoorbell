2fb81c50d830a516176a131adcb0ca9bd8f53aaf
Jeff DeGraw <nufsty2@gmail.com>
2020-01-15 21:44:57 -0700

    Rearange file system

---
+++ src_raspberryPi/start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Made by Jeff DeGraw for the DeafDoorbell project
+# 2020-01-15
+
+# This script starts the python flask server upon startup.
+# Simply put 
+
+sleep 10
+python3 pi_server.py
