e197cee8ae2a3a01b4836f2db4404c6a2b6af875
Jeff DeGraw <nufsty2@gmail.com>
2020-01-15 21:36:51 -0700

    Add blinking functionality

--- blink.py
+++ blink.py
@@ -1,45 +1,55 @@
+# Made by Jeff DeGraw for the DeafDoorbell project
+# 2020-01-15
+
+# Code for a routine that blinks the tpLink smart plugs
+
 from tplink_smartplug import SmartPlug
 import time
 
-# Set up plugs
-livingroom = SmartPlug('192.168.1.140')
-bedroom    = SmartPlug('192.168.1.139')
+"""
+This function will blink the two smart plugs at
+0.5 second intervals
+"""
+def start_blink():
+    # Set up plugs
+    livingroom = SmartPlug('192.168.1.140')
+    bedroom    = SmartPlug('192.168.1.139')
 
-# Get initial states of plugs
-livingroom_on_init = livingroom.is_on
-bedroom_on_init = bedroom.is_on
-# Copy initial states of plugs. This will make blinking much faster
-livingroom_on = livingroom_on_init
-bedroom_on = bedroom_on_init
+    # Get initial states of plugs
+    livingroom_on_init = livingroom.is_on
+    bedroom_on_init = bedroom.is_on
+    # Copy initial states of plugs. This will make blinking much faster
+    livingroom_on = livingroom_on_init
+    bedroom_on = bedroom_on_init
 
-# We need to keep track of time
-old_time = time.time()
-new_time = time.time()
-while (new_time - old_time < 30):
-    # Pause for 0.25 seconds, then increment time
-    time.sleep(0.5)
+    # We need to keep track of time
+    old_time = time.time()
     new_time = time.time()
+    while (new_time - old_time < 30):
+        # Pause for 0.25 seconds, then increment time
+        time.sleep(0.5)
+        new_time = time.time()
 
-    # Blink lights
-    if bedroom_on:
-        bedroom.turn_off()
-        bedroom_on = False
-    else:
-        bedroom.turn_on()
-        bedroom_on = True
-    if livingroom_on:
-        livingroom.turn_off()
-        livingroom_on = False
-    else:
+        # Blink lights
+        if bedroom_on:
+            bedroom.turn_off()
+            bedroom_on = False
+        else:
+            bedroom.turn_on()
+            bedroom_on = True
+        if livingroom_on:
+            livingroom.turn_off()
+            livingroom_on = False
+        else:
+            livingroom.turn_on()
+            livingroom_on = True
+        
+    # Make sure lights end how they started
+    if livingroom_on_init:
         livingroom.turn_on()
-        livingroom_on = True
-    
-# Make sure lights end how they started
-if livingroom_on_init:
-    livingroom.turn_on()
-else:
-    livingroom.turn_off()
-if bedroom_on_init:
-    bedroom.turn_on()
-else:
-    bedroom.turn_off()
+    else:
+        livingroom.turn_off()
+    if bedroom_on_init:
+        bedroom.turn_on()
+    else:
+        bedroom.turn_off()
--- pi_server.py
+++ pi_server.py
@@ -1,9 +1,20 @@
+# Made by Jeff DeGraw for the DeafDoorbell project
+# 2020-01-15
+
+# Code for a simple flask server that will receive 
+# commands from the ESP8266 and blink lights
+
 from flask import Flask
+import blink
 
 app = Flask(__name__)
 
+"""
+This function will respond to 
+"""
 @app.route('/ring')
 def ring():
+    blink.start_blink()
     return "Hello from the raspberry Pi!"
 
 app.run(host='0.0.0.0', port=8090)
