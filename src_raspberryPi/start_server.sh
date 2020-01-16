8166dfe8577e10e70d75fe1f46a665a81018126d
Jeff DeGraw <nufsty2@gmail.com>
2020-01-15 21:48:35 -0700

    Add doc to start_server.sh

--- src_raspberryPi/start_server.sh
+++ src_raspberryPi/start_server.sh
@@ -4,7 +4,9 @@
 # 2020-01-15
 
 # This script starts the python flask server upon startup.
-# Simply put 
+# Simply add the line:
+#   /home/pi/DeafDoorbell/src_raspberryPi/start_server.sh &
+# to the /etc/rc.local file to have this run upon boot
 
 sleep 10
 python3 pi_server.py
