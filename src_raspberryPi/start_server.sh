085f19cf52bb4684d69fcb7db3cbae17b7a72f27
Jeff DeGraw <nufsty2@gmail.com>
2020-01-15 22:31:49 -0700

    Fix start

--- src_raspberryPi/start_server.sh
+++ src_raspberryPi/start_server.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # Made by Jeff DeGraw for the DeafDoorbell project
 # 2020-01-15
 
 # This script starts the python flask server upon startup.
-# Simply add the line:
-#   /home/pi/DeafDoorbell/src_raspberryPi/start_server.sh &
-# to the /etc/rc.local file to have this run upon boot
+# Simply type "sudo crontab -e" add the line:
+#   @reboot sh /home/pi/DeafDoorbell/src_raspberryPi/start_server.sh >/home/pi/logs/server.log 2>&1
+# to cronjob file and ESC+:wq it.
 
 sleep 10
-runuser -l pi -c 'python3 /home/pi/DeafDoorbell/src_raspberryPi/pi_server.py >/home/pi/logs/server.log'
+su -c 'python3 /home/pi/DeafDoorbell/src_raspberryPi/pi_server.py >/home/pi/logs/server.log' nobody
